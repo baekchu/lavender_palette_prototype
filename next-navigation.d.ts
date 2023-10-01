60a57463a2ea49d22f82d709c4e6dcda5eeafcf0
Girae <138343237+Gitae-business@users.noreply.github.com>
2023-10-02 08:32:21 +0900

    1002 gitae 오류 수정

--- next-navigation.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'next-navigation' {
-  import { NextRouter } from 'next/router';
-
-  // next-navigation 모듈의 사용할 수 있는 멤버를 여기에 추가합니다.
-  export const useRouter: () => NextRouter;
-  // 다른 멤버들도 필요한 경우 추가하세요.
-}
